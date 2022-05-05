24e224cdfa243d2855d890053d29801f84016122
Teacher-droid <teaching.know@gmail.com>
2022-05-05 11:54:47 +0200

    who invented this thing

---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const Dotenv = require('dotenv-webpack');
+
+module.exports = (env) => {
+    console.log("NODE_ENV:", env.NODE_ENV);
+
+    return {
+
+        entry: './src/js/index.js',
+    
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js',
+            publicPath: '',
+        },
+    
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                },
+                {
+                    test: /\.(sa|sc|c)ss$/,
+                    use: [
+                        {
+                            loader: MiniCssExtractPlugin.loader,
+                        },
+                        {
+                            loader: 'css-loader',
+                        },                    
+                        {
+                            loader: 'postcss-loader',
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                implementation: require('sass'),
+                            },
+                        },
+    
+                    ],
+                },
+                {
+                    test: /\.(png|jpe?g|gif|svg)$/i,
+                    type: 'asset/resource',
+                },            
+                {
+                    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                    type: 'asset/resource',
+                },
+            ],
+        },
+    
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: 'bundle.css',
+            }),
+            new Dotenv(),
+        ],
+    
+        mode: 'development',
+    }
+}
